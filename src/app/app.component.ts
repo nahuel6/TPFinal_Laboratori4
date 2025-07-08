66d3ef34e55dd3ec6736672c29d154f2469eca72
Nahuel Mendoza <nahuelmendozaa@gmail.com>
2025-07-08 20:57:22 -0300

    actualizaciones visuales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent implements OnInit {
   constructor(private router: Router,private authService: AuthService ) {}
 
   ngOnInit(): void {
+   
     this.authService.userName$.subscribe(name => {
       this.userName = name;
     });
